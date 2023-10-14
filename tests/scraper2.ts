ad153bfb20bb9445bfdf91f908e848df872deadf
Micheal <86540880+MAYKELL07@users.noreply.github.com>
2023-10-14 02:23:34 +0000

    Work but CF

--- tests/scraper2.ts
+++ tests/scraper2.ts
@@ -1,8 +1,17 @@
 import { addExtra } from 'playwright-extra';
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth'
+import RecaptchaPlugin from 'puppeteer-extra-plugin-recaptcha';
 
+// Add the stealth plugin
 const stealth = StealthPlugin();
-const playwright = addExtra(require('playwright')).use(stealth);
+const recaptcha = RecaptchaPlugin({
+    provider: {
+      id: '2captcha',
+      token: '05550eb71909e8a303ff214ee64d24fd'  // Replace with your 2Captcha API key
+    },
+    visualFeedback: true // Colorize solved captchas for debugging
+});
+const playwright = addExtra(require('playwright')).use(stealth).use(recaptcha);
 
 async function scrapeYouChatText() {
     const browser = await playwright.firefox.launch({ headless: false });
@@ -11,9 +20,8 @@ async function scrapeYouChatText() {
     
     // Navigate to the target website
     await page.goto('https://you.com');
-    
-    // Wait for the textarea to be visible
-    await page.waitForSelector('#search-input-textarea', { state: 'visible' });
+
+    await page.solveCloudflareCaptcha(); 
 
     // Input text into the textarea
     await page.fill('#search-input-textarea', "what's your name");
@@ -27,10 +35,9 @@ async function scrapeYouChatText() {
     // Scrape the text
     const scrapedText = await page.textContent('[data-testid="youchat-text"]');
 
-    // Output the scraped text to the console (for debugging purposes)
+    // Output the scraped text to the console
     console.log(scrapedText);
 
-    // Close the incognito browser context
     await context.close();
 }
 
