8b207fba6b4007706079f3a10085af84c2b69e0c
Micheal <86540880+MAYKELL07@users.noreply.github.com>
2023-10-26 17:07:18 +0700

    Added Alodokter scrape

--- src/scraper/scraper.service.ts
+++ src/scraper/scraper.service.ts
@@ -10,7 +10,7 @@ export class ScraperService implements OnModuleInit, OnModuleDestroy {
 
     async onModuleInit() {
         const playwright = addExtra(require('playwright')).use(StealthPlugin());
-        this.browser = await playwright.chromium.launch({ headless: true });
+        this.browser = await playwright.chromium.launch({ headless: false });
         this.cookieData = await fs.readFile('src/scraper/cookie.json', 'utf8');
     }
 
@@ -40,15 +40,46 @@ export class ScraperService implements OnModuleInit, OnModuleDestroy {
             return responseText;
         }
     }
-    
-    async scrapeAlodokter(inputText: string): Promise<string> {
-        const context = await this.browser.newContext();
+
+    async scrapeAlodokter(inputText: string): Promise<any> {
+        const context = await this.browser.newContext({
+            userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'
+        });
         const page = await context.newPage();
-        await page.goto('https://www.alodokter.com');
+        await page.goto('https://www.alodokter.com', { waitUntil: 'domcontentloaded' });
+    
         await page.fill('#searchinput', inputText);
-        await page.click('div > div > div > a:nth-child(1)');
-        const paragraphText = await page.textContent('#postContent > p:nth-child(1)');
-        return paragraphText;
+        await page.keyboard.press('Enter');
+        
+        try {
+            await page.waitForSelector('#articles-result > card-post-index:nth-child(1)', { state: 'attached', timeout: 5000 });
+        } catch (error) {
+            console.error('Element not found:', error);
+            await context.close();
+            return null;
+        }
+    
+        const result = await page.evaluate(() => {
+            const card = document.querySelector('#articles-result > card-post-index:nth-child(1)');
+            if (card) {
+                return {
+                    title: card.getAttribute('title'),
+                    link: card.getAttribute('url-path'),
+                    image: card.getAttribute('image-url'),
+                    shortDescription: card.getAttribute('short-description')
+                };
+            } else {
+                return null;
+            }
+        });
+    
+        // Prepend "https://www.alodokter.com" to the link
+        if (result && result.link) {
+            result.link = `https://www.alodokter.com${result.link}`;
+        }
+    
+        await context.close();
+        return result;
     }
     
 }
